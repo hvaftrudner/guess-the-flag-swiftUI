c5da6d1955378077ee4d01905390292577b0faea
hvaftrudner <krill_eriksson@hotmail.com>
2021-01-28 00:00:48 +0100

    Initial Commit

---
+++ guessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  guessTheFlag
+//
+//  Created by Kristoffer Eriksson on 2021-01-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
